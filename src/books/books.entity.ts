dfedd6efae1fcb60f2256dac14433b05795389a5
KAPIQ03 <kapik0324@gmail.com>
2025-11-26 14:30:53 +0100

    initial commit

---
+++ src/books/books.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  category_idc: number;
+
+  @Column()
+  publisher_idp: number;
+}
